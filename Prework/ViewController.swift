89481b13de110d9e4e488766d10b7456b2b5928d
Michael Fang <MikeFang456@gmail.com>
2021-08-17 21:29:11 -0700

    Codepath - Prework

--- Prework/ViewController.swift
+++ Prework/ViewController.swift
@@ -9,11 +9,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var billAmountTextField: UITextField!
+    @IBOutlet weak var tipAmountLabel: UILabel!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var numOfPeople: UITextField!
+    @IBOutlet weak var totalPerPerson: UILabel!
+    @IBOutlet weak var totalPerPersonValue: UILabel!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
-
+    
+    //Calculate
+    @IBAction func calculateTip(_ sender: Any) {
+        let bill = Double(billAmountTextField.text!) ?? 0
+        
+        let people = Double(numOfPeople.text!) ?? 0
+        
+        let defaultTipPercentages = [0.10, 0.15, 0.20]
+        
+        
+        
+        let tip = bill * defaultTipPercentages[tipControl.selectedSegmentIndex]
+        let total = bill + tip
+        
+        let totalPerPerson = total / people
+        
+        tipAmountLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+        totalPerPersonValue.text = String(format: "$%.2f", totalPerPerson)
+        
+    }
+    
+    
 }
 
